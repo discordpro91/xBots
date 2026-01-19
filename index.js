793eeffbaac66ae6f711aa29f078243b9f593f7c
discordpro91 <doorsmoney795@gmail.com>
2026-01-19 00:23:57 -0500

    index.js

--- index.js
+++ index.js
@@ -20,29 +20,17 @@ const LOG_CHANNEL_ID = "1462677393381658750";
 
 function sendLog(guild, embed) {
   const channel = guild.channels.cache.get(LOG_CHANNEL_ID);
-  if (channel) channel.send({ embeds: [embed] }).catch(() => {});
+  if (channel) {
+    channel.send({ embeds: [embed] }).catch(() => {});
+  }
 }
 
 client.once("ready", () => {
   console.log(`✅ Logged in as ${client.user.tag}`);
 });
 
-client.on("messageDelete", async (message) => {
-  if (!message.guild || message.author?.bot) return;
+/* 
+  BOT ONLINE – ADD EVENTS BELOW
+*/
 
-  const embed = new EmbedBuilder()
-    .setTitle("🗑 Message Deleted")
-    .setColor("Red")
-    .addFields(
-      { name: "User", value: `${message.author}`, inline: true },
-      { name: "Channel", value: `${message.channel}`, inline: true },
-      { name: "Content", value: message.content || "*No content*" }
-    )
-    .setTimestamp();
-
-  sendLog(message.guild, embed);
-});
-
-client.on("messageUpdate", async (oldMsg, newMsg) => {
-  if
-}
+client.login(process.env.TOKEN);
